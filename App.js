45065344f92eba90bce78d916f2245c8444c2128
cleversamerr <thedev.samer@gmail.com>
2023-05-03 14:47:06 +0300

    updated android and ios bundles

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Icon name="rocket" size={30} color="#900" />
       <StatusBar style="auto" />
     </View>
   );
