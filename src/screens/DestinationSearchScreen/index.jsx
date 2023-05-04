97ab1f857f78cedef9669e4ddabb0613f9e21a85
cleversamerr <thedev.samer@gmail.com>
2023-05-04 11:56:33 +0300

    finished building the destination search screen

--- src/screens/DestinationSearchScreen/index.jsx
+++ src/screens/DestinationSearchScreen/index.jsx
@@ -1,36 +1,67 @@
 import { useState } from "react";
-import { View, StyleSheet, TextInput, SafeAreaView } from "react-native";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default function DestinationSearchScreen() {
-  const [context, setContext] = useState({ from: "", to: "" });
+  const [context, setContext] = useState({
+    originPlace: null,
+    destinationPlace: null,
+  });
 
-  const handleChange = (key) => (text) =>
-    setContext({ ...context, [key]: text });
+  const handleChange = (key, value) => {
+    setContext({ ...context, [key]: value });
+
+    const { originPlace, destinationPlace } = context;
+    if (originPlace && destinationPlace) {
+      // TODO: redirect to the next screen
+    }
+  };
 
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="From"
-          value={context.from}
-          onChangeText={handleChange("from")}
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            handleChange("originPlace", { data, details });
+          }}
+          query={{
+            key: "AIzaSyDVLeIyezNs3GmoqbQrKd3J1kWxt-ThQUc",
+            language: "en",
+          }}
         />
 
-        <TextInput
-          style={styles.textInput}
-          placeholder="Where to?"
-          value={context.to}
-          onChangeText={handleChange("to")}
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            handleChange("destinationPlace", { data, details });
+          }}
+          query={{
+            key: "AIzaSyDVLeIyezNs3GmoqbQrKd3J1kWxt-ThQUc",
+            language: "en",
+          }}
         />
       </View>
     </SafeAreaView>
   );
 }
 
+// AIzaSyDVLeIyezNs3GmoqbQrKd3J1kWxt-ThQUc
+
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    height: "100%",
   },
   textInput: {
     padding: 10,
